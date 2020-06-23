dbc14dad7f90d0074b44326af1391b5d4d847a65
Julia Cramer <julia.cramer@integritystl.com>
2020-06-23 13:54:28 -0500

    Adding the rest of the header css

--- wp-content/themes/acf-blocks/inc/custom-styles.php
+++ wp-content/themes/acf-blocks/inc/custom-styles.php
@@ -7,15 +7,15 @@ input,
 select,
 optgroup,
 textarea {
-	color: <?php the_typography_field('main_copy', 'text_color', 'option');?>;
-	font-family: <?php the_typography_field('main_copy', 'font_family', 'option');?>;
-	line-height: <?php the_typography_field('main_copy', 'line_height', 'option');?>;
-	font-size: <?php the_typography_field('main_copy', 'font_size', 'option');?>px;
-	font-weight: <?php the_typography_field('main_copy', 'font_weight', 'option');?>;
-	font-style: <?php the_typography_field('main_copy', 'font_style', 'option');?>;
-	letter-spacing: <?php the_typography_field('main_copy', 'letter_spacing', 'option');?>px;
-	text-decoration: <?php the_typography_field('main_copy', 'text_decoration', 'option');?>;
-	text-transform: <?php the_typography_field('main_copy', 'text_transform', 'option');?>;
+	color: <?php the_typography_field('body_copy', 'text_color', 'option');?>;
+	font-family: <?php the_typography_field('body_copy', 'font_family', 'option');?>;
+	line-height: <?php the_typography_field('body_copy', 'line_height', 'option');?>;
+	font-size: <?php the_typography_field('body_copy', 'font_size', 'option');?>px;
+	font-weight: <?php the_typography_field('body_copy', 'font_weight', 'option');?>;
+	font-style: <?php the_typography_field('body_copy', 'font_style', 'option');?>;
+	letter-spacing: <?php the_typography_field('body_copy', 'letter_spacing', 'option');?>px;
+	text-decoration: <?php the_typography_field('body_copy', 'text_decoration', 'option');?>;
+	text-transform: <?php the_typography_field('body_copy', 'text_transform', 'option');?>;
 }
 
 h1 {
@@ -28,4 +28,64 @@ h1 {
 	letter-spacing: <?php the_typography_field('header_1', 'letter_spacing', 'option');?>px;
 	text-decoration: <?php the_typography_field('header_1', 'text_decoration', 'option');?>;
 	text-transform: <?php the_typography_field('header_1', 'text_transform', 'option');?>;
+}
+
+h2 {
+	color: <?php the_typography_field('header_2', 'text_color', 'option');?>;
+	font-family: <?php the_typography_field('header_2', 'font_family', 'option');?>;
+	line-height: <?php the_typography_field('header_2', 'line_height', 'option');?>;
+	font-size: <?php the_typography_field('header_2', 'font_size', 'option');?>px;
+	font-weight: <?php the_typography_field('header_2', 'font_weight', 'option');?>;
+	font-style: <?php the_typography_field('header_2', 'font_style', 'option');?>;
+	letter-spacing: <?php the_typography_field('header_2', 'letter_spacing', 'option');?>px;
+	text-decoration: <?php the_typography_field('header_2', 'text_decoration', 'option');?>;
+	text-transform: <?php the_typography_field('header_2', 'text_transform', 'option');?>;
+}
+
+h3 {
+	color: <?php the_typography_field('header_3', 'text_color', 'option');?>;
+	font-family: <?php the_typography_field('header_3', 'font_family', 'option');?>;
+	line-height: <?php the_typography_field('header_3', 'line_height', 'option');?>;
+	font-size: <?php the_typography_field('header_3', 'font_size', 'option');?>px;
+	font-weight: <?php the_typography_field('header_3', 'font_weight', 'option');?>;
+	font-style: <?php the_typography_field('header_3', 'font_style', 'option');?>;
+	letter-spacing: <?php the_typography_field('header_3', 'letter_spacing', 'option');?>px;
+	text-decoration: <?php the_typography_field('header_3', 'text_decoration', 'option');?>;
+	text-transform: <?php the_typography_field('header_3', 'text_transform', 'option');?>;
+}
+
+h4 {
+	color: <?php the_typography_field('header_4', 'text_color', 'option');?>;
+	font-family: <?php the_typography_field('header_4', 'font_family', 'option');?>;
+	line-height: <?php the_typography_field('header_4', 'line_height', 'option');?>;
+	font-size: <?php the_typography_field('header_4', 'font_size', 'option');?>px;
+	font-weight: <?php the_typography_field('header_4', 'font_weight', 'option');?>;
+	font-style: <?php the_typography_field('header_4', 'font_style', 'option');?>;
+	letter-spacing: <?php the_typography_field('header_4', 'letter_spacing', 'option');?>px;
+	text-decoration: <?php the_typography_field('header_4', 'text_decoration', 'option');?>;
+	text-transform: <?php the_typography_field('header_4', 'text_transform', 'option');?>;
+}
+
+h5 {
+	color: <?php the_typography_field('header_5', 'text_color', 'option');?>;
+	font-family: <?php the_typography_field('header_5', 'font_family', 'option');?>;
+	line-height: <?php the_typography_field('header_5', 'line_height', 'option');?>;
+	font-size: <?php the_typography_field('header_5', 'font_size', 'option');?>px;
+	font-weight: <?php the_typography_field('header_5', 'font_weight', 'option');?>;
+	font-style: <?php the_typography_field('header_5', 'font_style', 'option');?>;
+	letter-spacing: <?php the_typography_field('header_5', 'letter_spacing', 'option');?>px;
+	text-decoration: <?php the_typography_field('header_5', 'text_decoration', 'option');?>;
+	text-transform: <?php the_typography_field('header_5', 'text_transform', 'option');?>;
+}
+
+h6 {
+	color: <?php the_typography_field('header_6', 'text_color', 'option');?>;
+	font-family: <?php the_typography_field('header_6', 'font_family', 'option');?>;
+	line-height: <?php the_typography_field('header_6', 'line_height', 'option');?>;
+	font-size: <?php the_typography_field('header_6', 'font_size', 'option');?>px;
+	font-weight: <?php the_typography_field('header_6', 'font_weight', 'option');?>;
+	font-style: <?php the_typography_field('header_6', 'font_style', 'option');?>;
+	letter-spacing: <?php the_typography_field('header_6', 'letter_spacing', 'option');?>px;
+	text-decoration: <?php the_typography_field('header_6', 'text_decoration', 'option');?>;
+	text-transform: <?php the_typography_field('header_6', 'text_transform', 'option');?>;
 }
