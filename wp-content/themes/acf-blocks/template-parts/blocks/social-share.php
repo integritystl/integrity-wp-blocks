8f47c47199a8ceddc03184f17ea6c6ecdfedb88f
Julia Cramer <julia.cramer@integritystl.com>
2020-07-06 14:55:13 -0500

    Social Share group

--- wp-content/themes/acf-blocks/template-parts/blocks/social-share.php
+++ wp-content/themes/acf-blocks/template-parts/blocks/social-share.php
@@ -7,10 +7,11 @@
 **/
 $socialHeadline = get_field('social_headline');
 $socialContent = get_field('social_content');
+$socialLayout = get_field('social_style');
 
 ?>
 
-<div class="social=share-block block-content">
+<div class="social-share-block block-content">
 
   <div class="social-share-wrapper">
   	<?php if($socialHeadline): ?>
@@ -21,31 +22,22 @@ $socialContent = get_field('social_content');
   	<?php endif; ?>
     <div class="block-content-section">
     	<!-- Begin Repeater -->
-		<?php if( have_rows('social_link') ): ?>
-			<?php while ( have_rows('social_link') ) : the_row();
-				$social = get_sub_field('social_link');
-				$social_link = $socialLink['url'];
-		    $social_title = $socialLink['title'];
-		    $social_target = $socialLink['target'] ? $social['target'] : '_self';
-		    $socialPlatform = get_sub_field('social_platform');	
-		    $socialStyle = get_sub_field('social_style');
-		    $socialStyleDark = $socialStyle['Dark'];
-			?>
-      	<?php if ($social): ?>
-    		<div class="social">
-    			<?php if( $socialStyle == 'linkedin' ) { ?>
-    			<a class="social-button <?php echo( $socialPlatform ); ?>-" href="<?php echo esc_url( $social_link ); ?>" target="_blaink"></a>
-    		</div>
-    		<?php } ?>
-
-	      	<?php endif; ?>
-
-
-	    <?php endwhile;?>
-
-		<?php endif; ?>
+      <div class="social <?php echo($socialLayout); ?>">
+  		<?php if( have_rows('social_link') ): ?>
+  			<?php while ( have_rows('social_link') ) : the_row();
+  				$social = get_sub_field('social_link');
+  				$social_link = $social['url'];
+  		    $social_title = $social['title'];
+  		    $social_target = $social['target'] ? $social['target'] : '_self';
+  		    $socialPlatform = get_sub_field('social_platform');	
+  			?>
+        	<?php if ($social): ?>
+      			<a class="social-button" href="<?php echo esc_url( $social_link ); ?>" target="_blaink"><i class="fab <?php echo($socialPlatform); ?>"></i></a>
+        	<?php endif; ?>
+        <?php endwhile;?>
+	    <?php endif; ?>
+      </div>
     </div>
-
   </div>
 
 </div>
